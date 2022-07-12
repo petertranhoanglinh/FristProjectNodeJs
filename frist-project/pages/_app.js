6079bf04bb9ff94d07988120d75c100d24a6b905
petertranhoanglinh <73209234+petertranhoanglinh@users.noreply.github.com>
2022-07-11 22:13:23 -0500

    edit package js

--- frist-project/pages/_app.js
+++ frist-project/pages/_app.js
@@ -2,6 +2,7 @@ import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
+  console.log('hello user new branch');
 }
 
 export default MyApp
